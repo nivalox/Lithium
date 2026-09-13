ca6694d77baccd14c25049a8d12696d664e2819c
Nivalos <nivaloz67@gmail.com>
2026-09-13 18:57:19 -0400

    Update static server to serve main.html

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import nodeStatic from 'node-static';
 const port = process.env.PORT || 8080;
 
 const bare = createBareServer('/bare/');
-const serve = new nodeStatic.Server('main/');
+const serve = new nodeStatic.Server('main/main.html');
 
 const server = http.createServer();
 
